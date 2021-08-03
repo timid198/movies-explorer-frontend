97e96a1d3c087efc4d07abe01664f5c6cdd9e489
Andrey Azovkin <adm.az-an@yandex.ru>
2021-08-03 22:30:44 +0300

    diplom front #1-6

--- src/components/NavTab/NavTab.js
+++ src/components/NavTab/NavTab.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import './NavTab.css';
+import { Link } from 'react-router-dom';
 
 function NavTab() {
     return (
-        <div></div>
+        <div className="navtab">
+            <div className="navtab-menu">
+            <Link to="/#point1" className="navtab-menu__point" >О проекте</Link>
+            <Link to="/#point2" className="navtab-menu__point" >Технологии</Link>
+            <Link to="/#point3" className="navtab-menu__point" >Студент</Link>
+                </div>
+        </div>
     )
 }
 
