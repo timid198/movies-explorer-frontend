d76a19fc8f2ea84462b7cc8af7dbd19b77f76913
Andrey Azovkin <adm.az-an@yandex.ru>
2021-08-04 19:58:49 +0300

    diplom front #1-7

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -3,7 +3,27 @@ import './AboutProject.css';
 
 function AboutProject() {
     return (
-        <div></div>
+        <div id="#point1" name="#point1" className="about-project">
+            <h2 className="about-project__title">О проекте</h2>
+            <div className="about-project__content">
+                <div className="about-project__stages">
+                    <h3 className="about-project__stages-title">Дипломный проект включал 5 этапов</h3>
+                    <p className="about-project__stages-content">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+                </div>
+                <div className="about-project__weeks">
+                    <h3 className="about-project__weeks-title">На выполнение диплома ушло 5 недель</h3>
+                    <p className="about-project__weeks-content">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+                </div>
+            </div>
+            <div className="about-project__infografic">
+                <div className="about-project__infografic-backend">1 неделя</div>
+                <div className="about-project__infografic-frontend">4 недели</div>
+            </div>
+            <div className="about-project__infografic-name">
+                <p className="about-project__infografic-name-backend">Back-end</p>
+                <p className="about-project__infografic-name-frontend">Front-end</p>
+            </div>
+        </div>
     )
 }
 
