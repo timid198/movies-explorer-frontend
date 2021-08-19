3b16b91b148067b589b9386ed7a4336f150ec626
Andrey Azovkin <adm.az-an@yandex.ru>
2021-08-19 23:09:27 +0300

    diplom front functional #1-5

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -4,17 +4,18 @@ import './MoviesCard.css';
 function MoviesCard({card}) {
 
 const like = `movies-card__like-place ${card.like && card.save ? 'movies-card__remove' : card.like ? 'movies-card__like' : 'movies-card__unlike'}`;
+console.log(card.image.url.formats)
 
     return (
         <div className="movies-card">
             <div className="movies-card__content">
                 <div className="movies-card__about">
-                    <h5 className="movies-card__title">{card.name}</h5>
+                    <h5 className="movies-card__title">{card.nameRU}</h5>
                     <p className="movies-card__duration">{card.duration}</p>
                 </div>
                 <button type="button" className={like} />
             </div>
-            <img src={card.image} alt={card.name} className="movies-card__image" />
+            <img src={`https://api.nomoreparties.co${card.image.url}`} alt={card.nameRU} className="movies-card__image" />
         </div>
     )
 }
